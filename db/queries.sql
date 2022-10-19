9515a705811e309a3fb356792f22c9544201c7c2
cspitt99 <104328339+cspitt99@users.noreply.github.com>
2022-10-19 00:11:17 -0400

    added package.json and ran npm i

--- db/queries.sql
+++ db/queries.sql
@@ -1,5 +1,9 @@
 USE employee_db;
 
+select * from department;
+
+select * from role;
+
 SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, CONCAT(mgr.first_name," ",mgr.last_name) as manager
 FROM employee
 LEFT JOIN role ON role.id=employee.role_id
